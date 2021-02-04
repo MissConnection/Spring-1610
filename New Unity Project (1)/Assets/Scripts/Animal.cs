c84fd0e32d70f97294b8ad1d978dab965d2b7af0
Lauren <laur.ashl.smith@gmail.com>
2021-02-04 13:18:41 -0700

    Ball Game added new script

--- New Unity Project (1)/Assets/Scripts/Animal.cs
+++ New Unity Project (1)/Assets/Scripts/Animal.cs
@@ -1,18 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public Color animalcolor = color.red;
+    public float speed = 1.0f;
+    public string animalName = "Bob";
 }
