8fd4a0414ef507bb9c69beb04d1175583620501f
Lauren <laur.ashl.smith@gmail.com>
2021-02-05 12:40:21 -0700

    added code

--- New Unity Project (1)/Assets/Scripts/Animal.cs
+++ New Unity Project (1)/Assets/Scripts/Animal.cs
@@ -5,4 +5,11 @@ public class Animal : MonoBehaviour
     public Color animalcolor = color.red;
     public float speed = 1.0f;
     public string animalName = "Bob";
+    public int health = 10;
+
+    private void start ()
+    {
+        throw new NotImplementedException();
+        Debug.Log(message:"Hello world.")
+    }
 }
