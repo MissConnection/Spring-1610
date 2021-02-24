6c1514de6c743a4e5641acbc12dc5bc1b75c11cf
Lauren <laur.ashl.smith@gmail.com>
2021-02-24 12:15:49 -0700

    added exploding script

---
+++ New Unity Project (1)/Assets/Scripts/Splode.cs
@@ -0,0 +1,15 @@
+﻿// Grenade explodes after a time delay.
+public float fuseTime;
+
+void Start()
+{
+    Invoke("Explode", fuseTime);
+}
+
+
+
+// Grenade explodes on impact.
+void OnCollisionEnter(Collision coll)
+{
+    Explode();
+}
---
+++ New Unity Project (1)/Assets/Scripts/fuseTime.cs
@@ -0,0 +1,17 @@
+﻿// Grenade explodes after a time delay.
+public Splode;
+
+void Start()
+{
+    {
+        Invoke("Explode", fuseTime);
+    }
+
+
+
+    // Grenade explodes on impact.
+    void OnCollisionEnter(Collision coll)
+    {
+        Explode();
+    }
+}
