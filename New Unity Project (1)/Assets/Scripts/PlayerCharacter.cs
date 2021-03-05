380019942e8f50eb4ece05213664a091d4dc4385
Lauren <laur.ashl.smith@gmail.com>
2021-03-05 10:41:31 -0700

    added script

---
+++ New Unity Project (1)/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    private float speed = 20.0f;
+    private float turnspeed = 45.0f
+    private float horizontalInput;
+    private float forwardInput;
+
+    void Update ()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical")
+        
+        transform.translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.up, turnspeed * horizontilInput * time.deltaTime);
+    }
+
+}
