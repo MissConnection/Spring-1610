fd1cb6e9f9a18c7bf9f7aad9d3f97642cec12dbf
Lauren <laur.ashl.smith@gmail.com>
2021-03-19 14:10:30 -0600

    tried to get things to explode failed

---
+++ Truck/Assets/Script/Splode.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Splode : MonoBehaviour
+{
+    public bool dead = false;
+    float deathRetryCounter = 0;
+}
