074fd4e68c3adbb49a89457456ab399a3388a407
Lauren <laur.ashl.smith@gmail.com>
2021-04-12 15:21:43 -0600

    added script

---
+++ Truck/Assets/Script/Die.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Die : MonoBehaviour
+{
+    public GameObject Square;
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "p")
+        {
+            Debug.Log("Collision detected");
+            Square.GetComponent<Renderer>().enabled = false;
+        }
+    }
+}
