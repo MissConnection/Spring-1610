568f336270212245e0a10c589c2b9f466e456247
Lauren <laur.ashl.smith@gmail.com>
2021-04-16 01:50:25 -0600

    deleted splode

--- Truck/Assets/Script/Splode.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Splode : MonoBehaviour
-{
-    public bool dead = false;
-    float deathRetryCounter = 0;
-}
